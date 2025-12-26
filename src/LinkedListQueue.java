323ca5503dfb38cfe828cd3c9ad6cbcd1f5bf21c
Hehe <i.am.still.learning@local>
2025-12-26 18:53:07 +0530

    v 0.9 Added linked list implementation of a linear queue

---
+++ src/LinkedListQueue.java
@@ -0,0 +1,86 @@
+public class LinkedListQueue {
+
+    private static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // initial state of the queue
+    private int size = 0;
+    private Node front = null;
+    private Node rear = null;
+
+    // general methods
+    public void enqueue(int data) {
+        Node newNode = new Node(data);
+
+        if(front == null) {
+            front = newNode;
+        }
+
+        if(rear == null) {
+            rear = newNode;
+            size++;
+            return;
+        }
+
+        rear.next = newNode;
+        rear = rear.next;
+        size++;
+    }
+    public int dequeue() {
+        if(isEmpty()) { // underflow check
+            System.out.println("Queue is empty... Dequeue failed");
+            return -1;
+        }
+
+        int removed = front.data; // temporarily store the data of the node to be removed
+        front = front.next;
+        size--;
+
+        if(front == null) { // queue is empty
+            rear = null;
+        }
+
+        return removed;
+    }
+    public int peek() {
+        if(isEmpty()) {
+            return -1;
+        }
+
+        return front.data;
+    }
+
+    // display
+    @Override
+    public String toString() {
+        if(isEmpty()) {
+            return "Queue is empty";
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        Node temp = front;
+        while(temp != null) {
+            sb.append(temp.data).append(" -> ");
+            temp = temp.next;
+        }
+        sb.append("End of the queue");
+
+        return sb.toString();
+    }
+
+    // utility methods
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    public int getSize() {
+        return size;
+    }
+}
---
+++ src/LinkedListQueueMain.java
@@ -0,0 +1,25 @@
+public class LinkedListQueueMain {
+    public static void main(String[] args) {
+
+        LinkedListQueue q = new LinkedListQueue();
+
+        q.enqueue(11);
+        q.enqueue(22);
+        q.enqueue(33);
+        q.enqueue(44);
+        q.enqueue(55);
+        q.enqueue(66);
+        q.dequeue();
+        q.dequeue();
+        q.dequeue();
+        q.enqueue(1);
+        q.enqueue(10);
+        q.enqueue(100);
+        q.enqueue(1000);
+
+        System.out.println("Element at the front: " + q.peek());
+        System.out.println("Current queue: " + q);
+        System.out.println("Size: " + q.getSize());
+        System.out.println("Is the queue empty?: " + q.isEmpty());
+    }
+}
