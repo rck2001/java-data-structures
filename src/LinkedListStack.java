bff63a5b72630705d1398ae705cce44dfca97fa6
Hehe <i.am.still.learning@local>
2025-12-26 20:24:09 +0530

    v 1.0 Implemented a linked list stack

---
+++ src/LinkedListStack.java
@@ -0,0 +1,71 @@
+// LIFO data structure
+public class LinkedListStack {
+
+    private static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // initial stack
+    int size = 0;
+    Node top = null;
+
+    // general methods
+    public void push(int data) {
+        Node newNode = new Node(data);
+
+        newNode.next = top;
+        top = newNode;
+        size++;
+    }
+    public int pop() {
+        if(isEmpty()) {
+            System.out.println("Stack is empty... Stack Underflow");
+            return -1;
+        }
+
+        int removed = top.data;
+        top = top.next;
+        size--;
+        return removed;
+    }
+    public int peek() {
+        if(isEmpty()) {
+            return -1;
+        }
+
+        return top.data;
+    }
+
+    // display
+    @Override
+    public String toString() {
+        if(isEmpty()) {
+            return "Stack is empty";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        Node temp = top;
+
+        while(temp != null) {
+            sb.append(temp.data).append(" -> \n");
+            temp = temp.next;
+        }
+        sb.append("NULL");
+
+        return sb.toString();
+    }
+
+    // utility methods
+    public boolean isEmpty() {
+        return size == 0; // or return top == null;
+    }
+    public int getSize() {
+        return size;
+    }
+}
---
+++ src/LinkedListStackMain.java
@@ -0,0 +1,24 @@
+public class LinkedListStackMain {
+    public static void main(String[] args) {
+
+        LinkedListStack stack = new LinkedListStack();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.pop(); // removes 4
+        stack.pop(); // removes 3
+        stack.push(5);
+        stack.push(6);
+        stack.push(7);
+        stack.push(8);
+        stack.pop(); // removes 8
+        stack.pop(); // removes 7
+
+        System.out.println(stack.peek());
+        System.out.println("Current stack: \n" + stack + "\n");
+        System.out.println("Size: " + stack.getSize());
+        System.out.println("Is the stack empty?: " + stack.isEmpty());
+    }
+}
