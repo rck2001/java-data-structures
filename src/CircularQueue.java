35cfbc89086b5effe32e972b01217bf00c0ad318
Hehe <i.am.still.learning@local>
2025-12-27 21:19:25 +0530

    v 1.1 Added circular queue

---
+++ src/CircularQueue.java
@@ -0,0 +1,85 @@
+public class CircularQueue {
+
+    // initial queue
+    private final int capacity;
+    private final int[] arr;
+    private int front, rear;
+    private int size;
+
+    public CircularQueue(int capacity) {
+        // initial queue
+        this.capacity = capacity;
+        this.arr = new int[capacity];
+        this.front = -1;
+        this.rear = -1;
+        this.size = 0;
+    }
+
+    // general methods
+    public void enqueue(int data) {
+        if(isFull()) {
+            System.out.println("Queue is full... Queue overflow");
+            return;
+        }
+
+        if(front == -1) {
+            front = 0;
+        }
+
+        rear = (rear + 1) % capacity;
+        arr[rear] = data;
+        size++;
+    }
+    public int dequeue() {
+        if(isEmpty()) {
+            System.out.println("Queue is empty... Queue underflow");
+            return -1;
+        }
+
+        int removed = arr[front];
+
+        if(size == 1) { // only one element is present
+            front = rear = -1; // resets the indices, i.e, the queue is now empty
+        }
+        else {
+            front = (front + 1) % capacity;
+        }
+
+        size--;
+        return removed;
+    }
+    public int peek() {
+        if(isEmpty()) {
+            return -1;
+        }
+
+        return arr[front];
+    }
+
+    // display
+    @Override
+    public String toString() {
+        if(isEmpty()) {
+            return "Queue is empty";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = front, count = 1; count <= size; count++, i = (i + 1) % capacity) {
+            sb.append(arr[i]).append(" -> ");
+        }
+        sb.append("End of queue");
+
+        return sb.toString();
+    }
+
+    // utility methods
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    public boolean isFull() {
+        return size == capacity;
+    }
+    public int getSize() {
+        return size;
+    }
+}
---
+++ src/CircularQueueMain.java
@@ -0,0 +1,26 @@
+public class CircularQueueMain {
+    public static void main(String[] args) {
+
+        CircularQueue q = new CircularQueue(5);
+
+        q.enqueue(22);
+        q.enqueue(44);
+        q.enqueue(88);
+        q.enqueue(55);
+        q.enqueue(99);
+        System.out.println(q.dequeue()); // removes 22
+        System.out.println(q.dequeue()); // removes 44
+        System.out.println(q.dequeue()); // removes 88
+        System.out.println(q.dequeue()); // removes 55
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(3);
+        q.enqueue(4);
+        q.enqueue(5);
+
+        System.out.println("Current queue: " + q);
+        System.out.println("Size: " + q.getSize());
+        System.out.println("Is the Queue empty?: " + q.isEmpty());
+        System.out.println("Is the Queue full?: " + q.isFull());
+    }
+}
