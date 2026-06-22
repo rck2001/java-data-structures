ff9416a628503bd54c10155b55bd0cebdd3ce940
rkhan <200360124+rck2001@users.noreply.github.com>
2026-06-22 16:18:43 +0530

    Hashmap improvements: 1. Added get, remove methods 2. Added dynamic resizing of buckets array 3. Fixed bug in hash method

--- src/demo/HashMapTest.java
+++ src/demo/HashMapTest.java
@@ -7,13 +7,17 @@ public class HashMapTest {
         HashMap map = new HashMap(5);
 
         map.put("Apple", 2);
-        map.put("banana", 3);
+        map.put("Banana", 51);
         map.put("Mango", 8);
         map.put("Orange", 22);
-        map.put("Dragon fruit", 44);
+        map.put("Dragon fruit", 22);
+        map.put("Something", 999);
+        map.put("Jackfruit", 432);
+        System.out.println("Removed value: " + map.remove("Apple"));
+        System.out.println("Value at the given key: " + map.get("Dragon fruit"));
 
-
-        System.out.println(map);
-        System.out.println("Size: " + map.size());
+        System.out.println("\nHashmap: " + map);
+        map.displayCollisionMapping();
+        System.out.println("\nCurrent size: " + map.size());
     }
 }
--- src/hashmap/HashMap.java
+++ src/hashmap/HashMap.java
@@ -14,49 +14,93 @@ public class HashMap {
     }
 
     // initial hashmap
-    private final Node[] buckets;
-    private final int capacity;
+    private int capacity;
+    private Node[] buckets;
     private int size = 0;
+    private final float loadFactor = 0.75f;
 
+    public HashMap() {
+        this.capacity = 16;
+        this.buckets = new Node[capacity];
+    }
     public HashMap(int capacity) {
         this.capacity = capacity;
         this.buckets = new Node[capacity];
     }
 
-    // hash method
-    private int hash(String key) {
-        return Math.abs(key.hashCode()) % capacity; // has Integer.MIN_VALUE bug
-    }
-
-    // put method
+    // put, remove and get method
     public void put(String key, int value) {
         int index = hash(key);
 
-        Node newNode = new Node(key, value);
-
-        if(buckets[index] != null) { // collision occurs… we do separate chaining to fix this
-            // Case 1: check for the same key in the bucket
-            Node current = buckets[index];
-            while(current != null) {
-                if(current.key.equals(key)) {
-                    current.value = value;
-                    return;
-                }
-
-                current = current.next;
+        // check if the key already exists
+        Node current = buckets[index];
+        while(current != null) {
+            if(current.key.equals(key)) {
+                current.value = value; // key found, update value
+                return; // exit
             }
 
-            newNode.next = buckets[index];
-            buckets[index] = newNode;
-            size++;
+            current = current.next;
         }
-        else { // when the bucket at the index is empty
-            buckets[index] = newNode;
-            size++;
+
+        // key not found
+        // create a key-value pair and add to the front
+        Node newNode = new Node(key, value);
+        newNode.next = buckets[index];
+        buckets[index] = newNode;
+
+        size++;
+
+        // CHECK IF THE BUCKETS ARRAY NEED RESIZING
+        if(size > (capacity * loadFactor)) {
+            resize();
         }
     }
+    public Integer remove(String key) {
+        int index = hash(key);
 
-    // display hashmap
+        Node current = buckets[index];
+        if(current == null) { // empty bucket
+            return null;
+        }
+        if(current.key.equals(key)) { // key present at first node
+            buckets[index] = buckets[index].next;
+            size--;
+
+            return current.value;
+        }
+
+        while(current.next != null) {
+            if(current.next.key.equals(key)) {
+                int removedValue = current.next.value;
+
+                current.next = current.next.next;
+                size--;
+
+                return removedValue;
+            }
+
+            current = current.next;
+        }
+
+        return null;
+    }
+    public Integer get(String key) {
+        int index = hash(key);
+
+        Node current = buckets[index];
+        while(current != null) {
+            if(current.key.equals(key)) {
+                return current.value;
+            }
+
+            current = current.next;
+        }
+
+        return null;
+    }
+
+    // display methods
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder("{");
@@ -88,5 +132,35 @@ public class HashMap {
         }
 
         size = 0;
+    }
+
+    // helpers
+    private int hash(String key) {
+        return (key.hashCode() & 0x7fffffff) % capacity;
+    }
+    private void resize() {
+        capacity *= 2;
+
+        Node[] OldBuckets = buckets;
+        buckets = new Node[capacity];
+
+        // we are reusing old nodes instead of creating new ones
+        for(int i = 0; i < OldBuckets.length; i++) {
+            Node current = OldBuckets[i];
+
+            while(current != null) {
+                Node nextOldNode = current.next;
+
+                // calculate new index based on new capacity
+                int newIndex = hash(current.key);
+
+                // insert nodes at the front
+                current.next = buckets[newIndex];
+                buckets[newIndex] = current;
+
+                // update the current to the next node in the old bucket list
+                current = nextOldNode;
+            }
+        }
     }
 }
